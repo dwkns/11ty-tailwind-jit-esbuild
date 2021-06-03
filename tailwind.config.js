7de57080e5ecb476d45320b674c82a54bcdc9404
dwkns <dwkns@me.com>
2021-06-03 14:29:01 +0100

    Starter improvements

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,6 @@
+const tailwindTheme = require('tailwindcss/defaultTheme');
+const debugScreens = require('tailwindcss-debug-screens');
+
 const brandRed = '#ee88a1';
 
 module.exports = {
@@ -9,14 +12,28 @@ module.exports = {
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1300px',
+      '2xl': '1500px',
+      '3xl': '1600px',
+    },
     extend: {
       colors: {
         'brand-red': brandRed,
+      },
+      fontFamily: {
+        'sans': ['Inter', ...tailwindTheme.fontFamily.sans],
+      },
+      spacing: {
+        'u2-pad': 'clamp(1.2rem, 3vw, 8rem)',
       }
     },
   },
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [debugScreens],
 }
